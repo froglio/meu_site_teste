90827cf22ba797d093b57c5051b5f745aeb30f5e
froglio <rogliofabricio@gmail.com>
2020-12-06 18:35:09 -0300

    deleteview criada

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new', views.BlogCreateView.as_view(), name='post_new'),
     path('post/<slug:slug>/<int:pk>', views.BlogDetailView.as_view(), name='post_detail'),
     path('post/<slug:slug>/<int:pk>/edit', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<slug:slug>/<int:pk>/delete', views.BlogDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from . models import Post
 
 
@@ -22,3 +23,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'blog/post_edit.html'
     fields = ['autor', 'titulo', 'conteudo', 'slug']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
